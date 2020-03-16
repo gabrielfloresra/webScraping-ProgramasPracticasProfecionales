eec319fc2e13a68641447ba6ce51efc001e6ae68
Gabriel Flores <gabriel.floresra@gmail.com>
2020-03-16 00:50:33 -0600

    cambiado el ancho del campo de la direccion

--- src/components/muestraProgramas.vue
+++ src/components/muestraProgramas.vue
@@ -132,14 +132,14 @@
                                                 </v-col>
                                                 <v-col cols="12" sm="12" md="6">
                                                     <v-text-field
-                                                        label="Domicilio"
-                                                        v-model="dataDialog.Domicilio"
-                                                    ></v-text-field>
-                                                </v-col>
-                                                <v-col cols="12" sm="12" md="6">
-                                                    <v-text-field
                                                         label="Sector"
                                                         v-model="dataDialog.SectorEmpresa"
+                                                    ></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12" sm="12" md="12">
+                                                    <v-text-field
+                                                        label="Domicilio"
+                                                        v-model="dataDialog.Domicilio"
                                                     ></v-text-field>
                                                 </v-col>
                                             </v-row>
